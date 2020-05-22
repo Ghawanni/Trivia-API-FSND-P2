0ae22991df676f2ccdc2159e0a8fcee6dcf43d48
Mohammed Ghawanni <m7md.ghawanni@gmail.com>
2020-05-22 18:44:53 +0300

    finished writing test cases for successful and failed attempts to use enpoints, project complete :rocket: :tada:

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,15 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question': 'Why did the chcken cross the road?',
+            'answer': 'To get to the other side',
+            'category': 1,
+            'difficulty': 5
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -30,10 +37,84 @@ class TriviaTestCase(unittest.TestCase):
         pass
 
     """
-    TODO
+    DONE:
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    def test_get_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['categories'])
+    def test_get_question_failed(self):
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['success'], False)
+
+
+    def test_create_question(self):
+        res = self.client().post('/questions', json=self.new_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['question'])
+    def test_create_question_failed(self):
+        res = self.client().post('/questions', json={'not_correct_params': False})
+        data = json.loads(res.data)
+
+        self.assertEqual(data['error'], 422)
+        self.assertEqual(data['success'], False)
+
+
+    def test_delete_question(self):
+        res = self.client().delete('/questions/2')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+    def test_delete_question_failed(self):
+        res = self.client().delete('/questions/2001')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+    
+
+    def test_search_term(self):
+        res = self.client().post('/questions?term=who')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['questions'])
+        self.assertTrue(data['total_questions'])
+    def test_search_term_failed(self):
+        res = self.client().post('/questions?term=owiefjewofij')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+
+    
+    def get_questions_by_category_test(self):
+        res = self.client().get('/categories/1/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['success'])
+        self.assertTrue(data['questions'])
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(data['current_category'])
+    def get_questions_by_category_test_failed(self):
+        res = sef.client().get('/categories/1/questions', json={'something_wrong': 'I shouldn\'t be here'})
+        data  = json.loads(res.data)
+        
+        self.assertEqual(data['error'], 404)
+        self.assertEqual(data['success'], False)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
